544871986eb5c6a4dea7ff13549ea5f7676fcdfa
Mengfanliang <2837247375@qq.com>
2022-02-13 22:43:16 +0800

    feat: 修改App.tsx，定义计数器案例demo

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h3>计数器案例demo</h3>
+      <div>点击次数: 0</div>
+      <br />
+      <button>increment</button>
+      <button>decrement</button>
+      <button>reset</button>
+    </>
   );
 }
 
