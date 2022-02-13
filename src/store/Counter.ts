fd5f577c396f2a16c361ac680f6e076f4211d66a
Mengfanliang <2837247375@qq.com>
2022-02-13 22:56:58 +0800

    fix: 第二次commit提交

---
+++ src/store/Counter.ts
@@ -0,0 +1,20 @@
+import { observable, action, makeObservable } from "mobx";
+
+class Counter {
+  constructor() {
+    /**
+   * Mobx@6版本中makeObservable的作用是创建可观察状态，其用法如下：
+   * 参数1：target把指定的参数变成响应式参数（可观察）
+   * 参数2：指定哪些属性或者方法变成可观察参数
+   */
+    makeObservable(this, {
+      count: observable
+    })
+  }
+
+  count = 0;
+}
+
+const counter = new Counter();
+
+export default counter;
